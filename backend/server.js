6f46599bee9dc93a6b69735e5fc72a42d79cb72c
vardhanguraza <vardhanguraza@users.noreply.github.com>
2024-01-09 20:37:57 -0600

    initial rest api for goals

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express =  require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
-const {errorHandler} = require('./middleware/errorMiddleware')
+const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const port = process.env.port || 5000;
 
+connectDB();
 const app = express();
 
 app.use(express.json());
